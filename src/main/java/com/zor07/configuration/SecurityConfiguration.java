b90a56b36d84e62a87ecdab14fc7f123d17a8167
anzor.karmov <anzor.karmov@loyaltyplant.com>
2017-08-04 18:52:52 +0300

    Made it work. i18n for login page and register page

--- src/main/java/com/zor07/configuration/SecurityConfiguration.java
+++ src/main/java/com/zor07/configuration/SecurityConfiguration.java
@@ -32,7 +32,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/resources/**", "/register").permitAll()
+                    .antMatchers("/resources/**", "/register", "/login/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
---
+++ src/main/java/com/zor07/configuration/WebMvcConfiguration.java
@@ -0,0 +1,48 @@
+package com.zor07.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Bean(name = "localeResolver")
+    public CookieLocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        Locale defaultLocale = new Locale("en");
+        localeResolver.setDefaultLocale(defaultLocale);
+        return localeResolver;
+    }
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:lang/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(10); //reload messages every 10 seconds
+        return messageSource;
+    }
+}
