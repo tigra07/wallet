9ee0b2cb6cc1acb894225c6cff1d836ccbf9f1d9
zor07 <anzor.karmov@gmail.com>
2017-10-01 20:45:27 +0300

    Fixed 404 for resources

--- src/main/java/com/zor07/configuration/WebMvcConfiguration.java
+++ src/main/java/com/zor07/configuration/WebMvcConfiguration.java
@@ -1,15 +1,22 @@
 package com.zor07.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.Ordered;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
+import java.util.List;
 import java.util.Locale;
 
 @Configuration
@@ -45,4 +52,42 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
         messageSource.setCacheSeconds(10); //reload messages every 10 seconds
         return messageSource;
     }
+
+//    @Override
+//    public void configurePathMatch(PathMatchConfigurer configurer) {
+//        super.configurePathMatch(configurer);
+//
+//        configurer.setUseSuffixPatternMatch(false);
+//    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        super.addResourceHandlers(registry);
+    }
+
+    //More configuration....
+
+    /* Here we register the Hibernate4Module into an ObjectMapper, then set this custom-configured ObjectMapper
+     * to the MessageConverter and return it to be added to the HttpMessageConverters of our application*/
+    public MappingJackson2HttpMessageConverter jacksonMessageConverter(){
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+
+        ObjectMapper mapper = new ObjectMapper();
+        //Registering Hibernate4Module to support lazy objects
+        mapper.registerModule(new Hibernate4Module());
+
+        messageConverter.setObjectMapper(mapper);
+        return messageConverter;
+
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //Here we add our custom-configured HttpMessageConverter
+        converters.add(jacksonMessageConverter());
+        super.configureMessageConverters(converters);
+    }
+
 }
